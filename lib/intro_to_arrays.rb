9ea80fa1c33ab84fc44329d8cbe1b76c7424cf8a
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-02-18 06:43:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,3 +6,15 @@ end
 def array_with_two_elements
   array = ["hi","hello"]
 end
+
+def first_element(array)
+  return array.first
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array.last
+end
